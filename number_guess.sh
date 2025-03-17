db559c43f37ffe26661b392b37b87581b6dacf45
Aniketh Nayak N <nayakaniketh970@gmail.com>
2025-03-17 07:11:12 +0000

    feat: Done with Main functionality

--- number_guess.sh
+++ number_guess.sh
@@ -16,4 +16,35 @@ if[[ -x $USERNAME_AVAIL ]]
 fi
 
 RANDOM_NUM=$((1 + $RANDOM % 1000))
+GUESS=1
 echo "Guess the secret number between 1 and 1000:"
+
+while read NUM
+do
+  if[[ ! $NUM =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  else
+    if[[ $NUM -eq $RANDOM_NUM ]]
+    then
+      break;
+    else
+      if[[ $NUM -gt $RANDOM_NUM ]]
+      then
+        echo "It's lower than that, guess again:"
+        elif[[ $NUM -lt $RANDOM_NUM ]]
+        then
+          echo "It's higher than that, guess again:"
+      fi
+    fi
+  fi
+  GUESS=$(( $GUESS+1 ))
+done
+
+
+if[[ $GUESS == 1 ]]
+then
+  echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
+else
+  echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
+fi
