faf15df7b296b4253b9a0fca816d017b9aa03bc2
Aniketh Nayak N <nayakaniketh970@gmail.com>
2025-03-17 06:55:22 +0000

    feat: Check Random function

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+echo "Enter your username:"
+read USERNAME
+
+USERNAME_AVAIL=$($PSQL "SELECT username FROM users where username='$USERNAME'")
+
+if[[ -x $USERNAME_AVAIL ]]
+  then
+    INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+  else
+    echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
+fi
+
+RANDOM_NUM=$((1 + $RANDOM % 1000))
+echo "Guess the secret number between 1 and 1000:"
